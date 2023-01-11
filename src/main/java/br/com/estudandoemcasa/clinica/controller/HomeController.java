fe959ae452d6f0c252a59319289fe26d6f369497
MiguelProgrammer <miguel.silva@mackenzista.com.br>
2023-01-11 17:47:43 -0300

    create the first end-point

--- src/main/java/br/com/estudandoemcasa/clinica/controller/HomeController.java
+++ src/main/java/br/com/estudandoemcasa/clinica/controller/HomeController.java
@@ -1,14 +1,22 @@
 package br.com.estudandoemcasa.clinica.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
+@RequestMapping("/")
 public class HomeController {
-    
-    @RequestMapping("/")
+
+    @GetMapping("/")
     public String home(){
         return "Hello World.";
+    }
+    @GetMapping("/developer")
+    public String developer(){
+        return "MiguelProgrammer";
     }
 }
