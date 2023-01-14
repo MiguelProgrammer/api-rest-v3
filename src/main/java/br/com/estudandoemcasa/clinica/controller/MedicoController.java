6ff24c9b91cb3f243e15d9467d668fe63c9f4c6f
MiguelProgrammer <miguel.silva@mackenzista.com.br>
2023-01-13 22:37:46 -0300

    Migrations com Flyway

--- src/main/java/br/com/estudandoemcasa/clinica/controller/MedicoController.java
+++ src/main/java/br/com/estudandoemcasa/clinica/controller/MedicoController.java
@@ -5,7 +5,11 @@ import br.com.estudandoemcasa.clinica.model.Doutor;
 import br.com.estudandoemcasa.clinica.repository.DoutorRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
@@ -15,6 +19,7 @@ public class MedicoController {
     @Autowired
     private DoutorRepository doutorRepository;
     @PostMapping
+    @Transactional
     public void cadastraMedico(@RequestBody Doutor doutor){
         log.info("Cadastrando novo médico = " + doutor.toString());
         doutorRepository.save(new DoutorEntity(doutor));
